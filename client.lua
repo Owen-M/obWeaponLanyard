b74b1ef851346de253801cdffc64126dce8646f1
Owen <owenbbmorgan@gmail.com>
2022-12-19 03:01:21 +0000

    Allow custom bones for addon weapons

--- client.lua
+++ client.lua
@@ -6,13 +6,13 @@ Citizen.CreateThread(function()
     while true do
         playerPed = PlayerPedId()
         playerWeapon = GetSelectedPedWeapon(playerPed)
-        
+
         if currentWeapon ~= playerWeapon then
             if hasLanyard then
                 TriggerServerEvent("obWeaponLanyard:removelanyard")
                 hasLanyard = false
             end
-                
+
             if isWeaponAllowed(playerWeapon) and not hasLanyard then
                 TriggerServerEvent("obWeaponLanyard:createlanyard")
                 hasLanyard = true
@@ -41,14 +41,20 @@ AddEventHandler("obWeaponLanyard:createlanyard", function(playerServerId)
     Citizen.Wait(250)
 
     player = GetPlayerPed(GetPlayerFromServerId(playerServerId))
-    playerWeapon = GetCurrentPedWeaponEntityIndex(player)    
+    playerWeapon = GetCurrentPedWeaponEntityIndex(player)
     playerCoords = GetEntityCoords(player)
     playerBone = GetEntityBoneIndexByName(player, 'SKEL_Pelvis')
     playerBoneCoords = GetPedBoneCoords(player, playerBone, vector3(0, 0, 0))
     playerBoneCoords = playerBoneCoords - playerCoords
 
+    weaponBone = 'gun_gripr'
+
+    if Config.CustomBones[playerWeapon] then
+        weaponBone = Config.CustomBones[playerWeapon]
+    end
+
     rope = AddRope(playerCoords, 0.0, 0.0, 0.0, 0.1, 5, 4.0, 0.001, 1.0, true, false, true, 1.0, false)
-    AttachEntitiesToRope(rope, player, playerWeapon, playerBoneCoords, vector3(0,0,0), 3.0, 0, 0, 'SKEL_Pelvis', 'gun_gripr')
+    AttachEntitiesToRope(rope, player, playerWeapon, playerBoneCoords, vector3(0,0,0), 3.0, 0, 0, 'SKEL_Pelvis', weaponBone)
 
     syncedRopes[playerServerId] = rope
 end)
--- config.lua
+++ config.lua
@@ -3,3 +3,7 @@ Config = {}
 Config.AcceptedWeapons = {
     `WEAPON_PISTOL`
 }
+
+Config.CustomBones = {
+    -- `SPAWN_CODE` = 'Custom Bone'
+}
